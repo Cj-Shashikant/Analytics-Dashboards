4aa8e5dbe068fc87feab9dc9f3c644bd8b0b791e
Cj-Shashikant <Shashikantt@kaitotech.com>
2025-10-16 19:07:33 +0530

    Add UI components index file to resolve import errors

--- out/_next/static/chunks/app/revenue/page-efc41d671e65e550.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[283],{35266:function(n,e,u){Promise.resolve().then(u.bind(u,10573))}},function(n){n.O(0,[562,458,351,273,573,971,23,744],function(){return n(n.s=35266)}),_N_E=n.O()}]);
